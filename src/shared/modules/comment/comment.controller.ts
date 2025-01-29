20a2d020c14d5c7c746813c778fe22c2961bfc7a
Evgeniy Timofeev <tev@maxil.org>
2025-01-29 15:24:15 +0300

    8. Ограничение доступа. Авторизация

--- src/shared/modules/comment/comment.controller.ts
+++ src/shared/modules/comment/comment.controller.ts
@@ -5,8 +5,9 @@ import {
   BaseController,
   HttpError,
   HttpMethod,
+  PrivateRouteMiddleware,
   ValidateDtoMiddleware,
-  ValidateObjectIdMiddleware,
+  ValidateObjectIdMiddleware
 } from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
@@ -37,6 +38,7 @@ export default class CommentController extends BaseController {
       method: HttpMethod.Post,
       handler: this.create,
       middlewares: [
+        new PrivateRouteMiddleware(),
         new ValidateDtoMiddleware(CreateCommentDto)
       ]
     });
@@ -52,7 +54,7 @@ export default class CommentController extends BaseController {
   }
 
   public async create(
-    { body }: Request<unknown, unknown, CreateCommentDto>,
+    { body, tokenPayload }: Request<unknown, unknown, CreateCommentDto>,
     res: Response
   ): Promise<void> {
     if (! await this.offerService.exists(body.offerId)) {
@@ -63,7 +65,7 @@ export default class CommentController extends BaseController {
       );
     }
 
-    const comment = await this.commentService.create(body);
+    const comment = await this.commentService.create({ ...body, userId: tokenPayload.id });
     await this.offerService.incCommentCount(body.offerId);
     await this.offerService.recalculateRating(body.offerId);
     this.created(res, fillDTO(CommentRdo, comment));
